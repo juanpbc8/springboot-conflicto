451465bddad50c7bdde999fde50822dc00264dee
juanp <jbaldeoncharri@gmail.com>
2025-06-18 16:40:51 -0500

    Primer commit

---
+++ src/main/java/pe/edu/utp/catalogo/DemoConflictoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConflictoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConflictoApplication.class, args);
+	}
+
+}
